b50ecb1c7091ac2fba75fee95a1f3098c2e5aa79
Helson Matos <helson.11@hotmail.com>
2023-08-18 11:46:09 -0300

    cadastro de animais só pelo nome

--- public/index.js
+++ public/index.js
@@ -15,11 +15,27 @@ async function carregarAnimais(){
 
 function manipularFormulario(){
     const form_animal = document.getElementById('form-animal')
+    const input_nome = document.getElementById('nome')
+    
+    form_animal.onsubmit = async (event)=> {
+        event.preventDefault()
+        const nome_animal = input_nome.value
+
+        await axios.post('http://127.0.0.1:8000/animais', {
+            id: 0,
+            nome: nome_animal,
+            idade: 2,
+            sexo: 'macho',
+            cor: 'branco'
+        })
+    }
+    
 }
 
 function app(){
     console.log("app iniciado")
     carregarAnimais()
+    manipularFormulario()
 }
 
 app = app()
